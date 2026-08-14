00f8609687211afc56406bff6bd3c80f92d20c7e
milansgr <47173460+milansgr@users.noreply.github.com>
2026-08-14 06:35:34 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "SAH MILOXX QUEL GOAT",     // this is what people actually see
+        state: "𝔱𝔢𝔠𝔥𝔫𝔬𝔬𝔬𝔬𝔬𝔬𝔬𝔬 !",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
